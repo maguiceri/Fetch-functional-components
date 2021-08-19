7855f65745c028fdec4e7b76c1b948136dd81d5d
magui <magui@Maguis-MacBook-Pro.local>
2021-08-19 17:55:57 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
 function App() {
+  const [data, setData] = useState({})
+
+  async function fetchData() {
+    const getData = await fetch("https://api.github.com/users/juanigallo")
+    const getJson = await getData.json()
+
+    setData(getJson)
+    
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Login:{data.login}
+    </>
   );
 }
 
